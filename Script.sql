9f284cac33583f5859e0e31f4f7b067b4146e0c9
Matheus Francisco <145405008+theusjf@users.noreply.github.com>
2026-03-09 19:40:12 -0300

    Update Script.sql

--- Script.sql
+++ Script.sql
@@ -1,43 +1,43 @@
+-- tabela advisor
 SELECT COUNT(*) FROM advisor;
-
 -- 2000 registros 
 
+-- tabela classroom
 SELECT COUNT(*) FROM classroom;
-
 -- 30 registros 
 
+-- tabela course
 SELECT COUNT(*) FROM course;
-
 -- 200 registros
 
+-- tabela department
 SELECT COUNT(*) FROM department;
-
 -- 20 registros
 
+-- tabela instructor
 SELECT COUNT(*) FROM instructor;
-
 -- 50 registros
 
+-- tabela prereq
 SELECT COUNT(*) FROM prereq;
-
 -- 100 registros
 
+-- tabela section
 SELECT COUNT(*) FROM section;
-
 -- 100 registros
 
+-- tabela student
 SELECT COUNT(*) FROM student;
-
 -- 2000 registros
 
+-- tabela takes
 SELECT COUNT(*) FROM takes;
-
 -- 30000 registros
 
+-- tabela teaches
 SELECT COUNT(*) FROM teaches;
-
 -- 100 registros
 
+-- tabela time_slot
 SELECT COUNT(*) FROM time_slot;
-
 -- 20 registros
