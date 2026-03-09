bf8ec4f99d06a50e6f2d60d636a21b622a6d5c00
Matheus Francisco <145405008+theusjf@users.noreply.github.com>
2026-03-09 19:37:48 -0300

    Add files via upload

---
+++ Script.sql
@@ -0,0 +1,43 @@
+SELECT COUNT(*) FROM advisor;
+
+-- 2000 registros 
+
+SELECT COUNT(*) FROM classroom;
+
+-- 30 registros 
+
+SELECT COUNT(*) FROM course;
+
+-- 200 registros
+
+SELECT COUNT(*) FROM department;
+
+-- 20 registros
+
+SELECT COUNT(*) FROM instructor;
+
+-- 50 registros
+
+SELECT COUNT(*) FROM prereq;
+
+-- 100 registros
+
+SELECT COUNT(*) FROM section;
+
+-- 100 registros
+
+SELECT COUNT(*) FROM student;
+
+-- 2000 registros
+
+SELECT COUNT(*) FROM takes;
+
+-- 30000 registros
+
+SELECT COUNT(*) FROM teaches;
+
+-- 100 registros
+
+SELECT COUNT(*) FROM time_slot;
+
+-- 20 registros
